99113c4296976ddcb8b05d51ad12f459ad51da06
Toodie <lucas.vtodt@gmail.com>
2024-07-03 09:38:17 -0300

    update main.py to contain test flow

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+from random import random
+
+from src.sample.sample import Sample
+
 def hello_world():
     return "Hello World!"
 
 if __name__ == "__main__":
-    print(hello_world())
+    rol = []
+    for i in range(100):
+        rol.append(round(random() * 2500))
+    rol.sort()
+    sample = Sample(rol)
+    sample.setup()
+    print(sample.show_main_data_formatted())
+    print(sample.show_data_formatted())
